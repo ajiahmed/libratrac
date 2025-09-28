f07295cb49cb64d310ca9ec5d19e04f11862ed8f
moh techpreneur <60311337+ajiahmed@users.noreply.github.com>
2025-09-28 22:08:28 +0100

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+
 import { motion } from "framer-motion";
 import { Bell, BookOpen, Users, AlertTriangle } from "lucide-react";
 ‎
